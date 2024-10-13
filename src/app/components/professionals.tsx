10a22bb5e524c89d4bb7d19a3d18ad8630ca7ed0
Miguel-Edson <migueledson.11@gmail.com>
2024-10-12 23:17:04 -0300

    :iphone: feat: Fixed responsivity of members component

--- src/app/components/professionals.tsx
+++ src/app/components/professionals.tsx
@@ -6,22 +6,22 @@ import Image from 'next/image'
 export function Professionals() {
   return (
     <div className='w-full h-[64rem] bg-img_bg_professionals bg-cover justify-center flex items-center my-auto '>
-        <div className='w-[58.5rem] h-[40rem] flex flex-col items-center justify-between mx-auto'>
+        <div className='md:w-[58.5rem] w-full md:mx-auto mx-[2rem] md:h-[40rem] md:py-0 py-[2rem] h-full flex flex-col items-center justify-between '>
         
             <div className='text-white w-full'>
-                <h1 className='text-3xl pb-[1rem]'> 
+                <h1 className='text-3xl pb-[1rem] font-abril'> 
                     Nosso Time 
                 </h1>
-                <h3 className='text-sm pb-[2rem]'> 
+                <h3 className='text-sm pb-[2rem] font-sourceSerif'> 
                     Conheça nosso time de profissionais que irão te acompanhar nessa jornada de aprendizado.        
                 </h3>
                 
-                <div className='flex justify-between w-full'>
+                <div className='flex flex-wrap justify-between w-full gap-y-4'>
 
                     <div className="relative group">
                         <a href='#' >
                                 <Image
-                                    className='rounded-xl border-4 border-white'
+                                    className='rounded-xl border-4 border-white md:w-[12rem] w-[9rem] md:h-[12rem] h-[9rem] '
                                     alt='https://www.freepik.com/free-photo/front-view-male-barber-holding-scissors-barbershop_14833851.htm#fromView=search&page=1&position=39&uuid=d462b5f2-2cd5-4774-8f53-33bcb9a3e3f8'
                                     src={"/assets/barber1.jpg"}
                                     width={200}
@@ -37,7 +37,7 @@ export function Professionals() {
                     <div className="relative group">
                         <a href='#' >
                                 <Image
-                                    className='rounded-xl border-4 border-white'
+                                    className='rounded-xl border-4 border-white md:w-[12rem] w-[9rem] md:h-[12rem] h-[9rem] '
                                     alt='https://www.freepik.com/free-photo/front-view-male-barber-holding-scissors-barbershop_14833851.htm#fromView=search&page=1&position=39&uuid=d462b5f2-2cd5-4774-8f53-33bcb9a3e3f8'
                                     src={"/assets/barber2.jpg"}
                                     width={200}
@@ -53,7 +53,7 @@ export function Professionals() {
                     <div className="relative group">
                         <a href='#' >
                                 <Image
-                                    className='rounded-xl border-4 border-white'
+                                    className='rounded-xl border-4 border-white md:w-[12rem] w-[9rem] md:h-[12rem] h-[9rem] '
                                     alt='https://www.freepik.com/free-photo/front-view-male-barber-holding-scissors-barbershop_14833851.htm#fromView=search&page=1&position=39&uuid=d462b5f2-2cd5-4774-8f53-33bcb9a3e3f8'
                                     src={"/assets/barber3.jpg"}
                                     width={200}
@@ -69,7 +69,7 @@ export function Professionals() {
                     <div className="relative group">
                         <a href='#' >
                                 <Image
-                                    className='rounded-xl border-4 border-white'
+                                    className='rounded-xl border-4 border-white md:w-[12rem] w-[9rem] md:h-[12rem] h-[9rem] '
                                     alt='https://www.freepik.com/free-photo/front-view-male-barber-holding-scissors-barbershop_14833851.htm#fromView=search&page=1&position=39&uuid=d462b5f2-2cd5-4774-8f53-33bcb9a3e3f8'
                                     src={"/assets/barber4.jpg"}
                                     width={200}
@@ -90,7 +90,7 @@ export function Professionals() {
                 </div>
             </div>
 
-            <div className='text-white flex flex-col'>
+            <div className='text-white flex flex-col my-[3rem] md:my-0 '>
                 <h1 className='text-3xl text-center pb-[2rem]'> 
                     Um pouco sobre a BARBER’S SCHOOL
                 </h1>
